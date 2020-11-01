b3f7f933399b095acadec97be02b2ad86649fa75
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2020-11-01 13:52:46 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    double num1,num2;
+    cout<<"Enter First Number: ";
+    cin>>num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;
+    cout<<"Sum of two number is :"<<num1+num2<<endl;
+    cout<<"Enter First Number: ";
+    cin>>num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;
+    cout<<"Division of two number is :"<<num1/num2<<endl;
+    cout<<"Enter First Number: ";
+    cin>>num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;
+    cout<<"Multification of two number is :"<<num1*num2<<endl;
+    cout<<"Enter First Number: ";
+    cin>>num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;
+    cout<<"Subtraction of two number is :"<<num1-num2<<endl;
+
+    return 0;
+
+}
